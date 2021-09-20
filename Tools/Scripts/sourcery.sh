46ebd1fbc125a39f43f6363894eba2d6b39c3111
yyokii <yyokii.h@gmail.com>
2021-09-20 21:37:15 +0900

    🩹 Fix code generate script

--- Tools/Scripts/sourcery.sh
+++ Tools/Scripts/sourcery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-../Sourcery/sourcery \
+../CodeGenerator/Sourcery/sourcery \
    --sources $SCRIPT_INPUT_FILE_0 \
    --templates ../CodeGenerator/Templates/ \
    --output $SCRIPT_OUTPUT_FILE_0 \
