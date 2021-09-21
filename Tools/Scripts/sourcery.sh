27d1020b56b15feda34a8c6d57e426edd7d9da93
yyokii <yyokii.h@gmail.com>
2021-09-21 21:55:44 +0900

    🩹

--- Tools/Scripts/sourcery.sh
+++ Tools/Scripts/sourcery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-{BUILD_DIR%Build/*}SourcePackages/checkouts/UIPreviewCatalog/Tools/CodeGenerator/Sourcery/sourcery \
+${BUILD_DIR%Build/*}SourcePackages/checkouts/UIPreviewCatalog/Tools/CodeGenerator/Sourcery/sourcery \
    --sources $SCRIPT_INPUT_FILE_0 \
    --templates ../CodeGenerator/Templates/ \
    --output $SCRIPT_OUTPUT_FILE_0 \
