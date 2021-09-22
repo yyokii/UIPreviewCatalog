45933145a6da8163ff395b7906ffc08022ed8c4f
yyokii <yyokii.h@gmail.com>
2021-09-22 12:19:45 +0900

    🩹 Add @testable import to Stencil

--- Tools/Scripts/generate_preview_item_code.sh
+++ Tools/Scripts/generate_preview_item_code.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Set the target name as an argument."
+  exit 1
+fi
+
 ${BUILD_DIR%Build/*}SourcePackages/checkouts/UIPreviewCatalog/Tools/CodeGenerator/Sourcery/sourcery \
    --sources $SCRIPT_INPUT_FILE_0 \
    --templates ${BUILD_DIR%Build/*}SourcePackages/checkouts/UIPreviewCatalog/Tools/CodeGenerator/Templates \
    --output $SCRIPT_OUTPUT_FILE_0 \
+   --args mainTarget=$1
